9ea18bba903341710933d669f443185e2ea89f3b
Mike Kornegay <mjkorneg@gmail.com>
2021-10-18 10:06:16 -0400

    Add project files.

---
+++ FileDisplaySample/Controllers/FileDisplayController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileDisplaySample.Controllers
+{
+    public class FileDisplayController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public FileContentResult ShowFile()
+        {
+            //get the content type
+            string fileName = "C:\\Users\\Mkornegay\\Downloads\\sample3.pdf";
+            string contentType = "";
+            new FileExtensionContentTypeProvider().TryGetContentType(fileName, out contentType);
+
+            //get the bytes
+            byte[] fileContents = System.IO.File.ReadAllBytes(fileName);
+
+
+            return new FileContentResult(fileContents, contentType);
+        }
+
+
+    }
+}
